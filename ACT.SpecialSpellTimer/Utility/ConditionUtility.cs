1e1c531850f85a682c4ab2002299dde0b6b6e2d3
anoyetta <anoyetta@gmail.com>
2017-06-27 15:43:12 +0900

    整形

--- ACT.SpecialSpellTimer/Utility/ConditionUtility.cs
+++ ACT.SpecialSpellTimer/Utility/ConditionUtility.cs
@@ -1,11 +1,8 @@
 ﻿namespace ACT.SpecialSpellTimer.Utility
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Text;
     using System.Text.RegularExpressions;
-    using System.Threading.Tasks;
 
     using ACT.SpecialSpellTimer.Models;
 
@@ -16,9 +13,12 @@
         /// </summary>
         /// <param name="spell">SpellTimer</param>
         /// <returns>条件を満たしていればtrue</returns>
-        public static bool CheckConditionsForSpell(SpellTimer spell)
+        public static bool CheckConditionsForSpell(
+            SpellTimer spell)
         {
-            return CheckConditions(spell.TimersMustRunningForStart, spell.TimersMustStoppingForStart);
+            return CheckConditions(
+                spell.TimersMustRunningForStart,
+                spell.TimersMustStoppingForStart);
         }
 
         /// <summary>
@@ -26,9 +26,31 @@
         /// </summary>
         /// <param name="telop">OnePointTelop</param>
         /// <returns>条件を満たしていればtrue</returns>
-        public static bool CheckConditionsForTelop(OnePointTelop telop)
+        public static bool CheckConditionsForTelop(
+            OnePointTelop telop)
         {
-            return CheckConditions(telop.TimersMustRunningForStart, telop.TimersMustStoppingForStart);
+            return CheckConditions(
+                telop.TimersMustRunningForStart,
+                telop.TimersMustStoppingForStart);
+        }
+
+        /// <summary>
+        /// 後方参照を置換したMessageを返す
+        /// </summary>
+        /// <param name="telop">OnePointTelop</param>
+        /// <returns>置換後のMessage</returns>
+        public static string GetReplacedMessage(
+            OnePointTelop telop)
+        {
+            var builder = new StringBuilder(telop.Message);
+
+            int index = 1;
+            index += ReplaceMessageWithSpell(builder, telop.TimersMustRunningForStart, index);
+            index += ReplaceMessageWithSpell(builder, telop.TimersMustStoppingForStart, index);
+            index += ReplaceMessageWithTelop(builder, telop.TimersMustRunningForStart, index);
+            index += ReplaceMessageWithTelop(builder, telop.TimersMustStoppingForStart, index);
+
+            return builder.ToString();
         }
 
         /// <summary>
@@ -36,7 +58,8 @@
         /// </summary>
         /// <param name="spell">SpellTimer</param>
         /// <returns>置換後のTitle</returns>
-        public static string GetReplacedTitle(SpellTimer spell)
+        public static string GetReplacedTitle(
+            SpellTimer spell)
         {
             var builder = new StringBuilder(spell.SpellTitle);
 
@@ -50,102 +73,14 @@
         }
 
         /// <summary>
-        /// 後方参照を置換したMessageを返す
-        /// </summary>
-        /// <param name="telop">OnePointTelop</param>
-        /// <returns>置換後のMessage</returns>
-        public static string GetReplacedMessage(OnePointTelop telop)
-        {
-            var builder = new StringBuilder(telop.Message);
-            
-            int index = 1;
-            index += ReplaceMessageWithSpell(builder, telop.TimersMustRunningForStart, index);
-            index += ReplaceMessageWithSpell(builder, telop.TimersMustStoppingForStart, index);
-            index += ReplaceMessageWithTelop(builder, telop.TimersMustRunningForStart, index);
-            index += ReplaceMessageWithTelop(builder, telop.TimersMustStoppingForStart, index);
-
-            return builder.ToString();
-        }
-
-        /// <summary>
-        /// 指定されたGuidに対応するSpellTimerの後方参照で置換を行う
-        /// 置換文字列は "$C" + 条件の出現順（1～） + "-" + 後方参照の番号
-        /// 例： $C1-2（一つ目の条件となるタイマーの二つ目のグループに置換）
-        /// </summary>
-        /// <param name="message">元の文字列</param>
-        /// <param name="timers">置換候補のタイマー</param>
-        /// <param name="baseIndex">条件の出現順の開始番号</param>
-        /// <returns>候補となったタイマーの個数</returns>
-        private static int ReplaceMessageWithSpell(StringBuilder message, Guid[] timers, int baseIndex)
-        {
-            int count = 0;
-
-            for (int i = 0; i < timers.Length; i++)
-            {
-                var spell = SpellTimerTable.GetSpellTimerByGuid(timers[i]);
-                if (spell != null)
-                {
-                    count++;
-
-                    if (spell.RegexEnabled && spell.Regex != null && spell.MatchedLog != string.Empty)
-                    {
-                        foreach (Match match in spell.Regex.Matches(spell.MatchedLog))
-                        {
-                            foreach (var number in spell.Regex.GetGroupNumbers())
-                            {
-                                message.Replace(String.Format("$C{0}-{1}", (baseIndex + i), number), match.Groups[number].Value);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return count;
-        }
-
-        /// <summary>
-        /// 指定されたGuidに対応するOnePointTelopの後方参照で置換を行う
-        /// 置換文字列は "$C" + 条件の出現順（1～） + "-" + 後方参照の番号
-        /// 例： $C1-2（一つ目の条件となるタイマーの二つ目のグループに置換）
-        /// </summary>
-        /// <param name="message">元の文字列</param>
-        /// <param name="timers">置換候補のタイマー</param>
-        /// <param name="baseIndex">条件の出現順の開始番号</param>
-        /// <returns>候補となったタイマーの個数</returns>
-        private static int ReplaceMessageWithTelop(StringBuilder message, Guid[] timers, int baseIndex)
-        {
-            int count = 0;
-
-            for (int i = 0; i < timers.Length; i++)
-            {
-                var telop = OnePointTelopTable.Default.GetOnePointTelopByGuid(timers[i]);
-                if (telop != null)
-                {
-                    count++;
-
-                    if (telop != null && telop.RegexEnabled && telop.Regex != null && !string.IsNullOrEmpty(telop.MatchedLog))
-                    {
-                        foreach (Match match in telop.Regex.Matches(telop.MatchedLog))
-                        {
-                            foreach (var number in telop.Regex.GetGroupNumbers())
-                            {
-                                message.Replace(String.Format("$C{0}-{1}", (baseIndex + i), number), match.Groups[number].Value);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return count;
-        }
-
-        /// <summary>
         /// 指定された全てのTimerが条件を満たしているか確認する
         /// </summary>
         /// <param name="timersMustRunning">稼働中であることが求められているTimerの配列</param>
         /// <param name="timersMustStopping">停止中であることが求められているTimerの配列</param>
         /// <returns>全てのTimerが条件を満たしていればtrue</returns>
-        private static bool CheckConditions(Guid[] timersMustRunning, Guid[] timersMustStopping)
+        private static bool CheckConditions(
+            Guid[] timersMustRunning,
+            Guid[] timersMustStopping)
         {
             if (timersMustRunning.Length == 0 && timersMustStopping.Length == 0)
             {
@@ -193,7 +128,8 @@
         /// </summary>
         /// <param name="spell">SpellTimer</param>
         /// <returns>稼働中であればtrue</returns>
-        private static bool IsRunning(SpellTimer spell)
+        private static bool IsRunning(
+            SpellTimer spell)
         {
             var recastTime = (spell.CompleteScheduledTime - DateTime.Now).TotalSeconds;
             return recastTime >= 0;
@@ -205,7 +141,8 @@
         /// </summary>
         /// <param name="telop">OnePointTelop</param>
         /// <returns>稼働中であればtrue</returns>
-        private static bool IsRunning(OnePointTelop telop)
+        private static bool IsRunning(
+            OnePointTelop telop)
         {
             if (telop.MatchDateTime > DateTime.MinValue && !telop.ForceHide)
             {
@@ -217,7 +154,86 @@
                     return true;
                 }
             }
+
             return false;
+        }
+
+        /// <summary>
+        /// 指定されたGuidに対応するSpellTimerの後方参照で置換を行う
+        /// 置換文字列は "$C" + 条件の出現順（1～） + "-" + 後方参照の番号
+        /// 例： $C1-2（一つ目の条件となるタイマーの二つ目のグループに置換）
+        /// </summary>
+        /// <param name="message">元の文字列</param>
+        /// <param name="timers">置換候補のタイマー</param>
+        /// <param name="baseIndex">条件の出現順の開始番号</param>
+        /// <returns>候補となったタイマーの個数</returns>
+        private static int ReplaceMessageWithSpell(
+            StringBuilder message,
+            Guid[] timers,
+            int baseIndex)
+        {
+            int count = 0;
+
+            for (int i = 0; i < timers.Length; i++)
+            {
+                var spell = SpellTimerTable.GetSpellTimerByGuid(timers[i]);
+                if (spell != null)
+                {
+                    count++;
+
+                    if (spell.RegexEnabled && spell.Regex != null && spell.MatchedLog != string.Empty)
+                    {
+                        foreach (Match match in spell.Regex.Matches(spell.MatchedLog))
+                        {
+                            foreach (var number in spell.Regex.GetGroupNumbers())
+                            {
+                                message.Replace(String.Format("$C{0}-{1}", (baseIndex + i), number), match.Groups[number].Value);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// 指定されたGuidに対応するOnePointTelopの後方参照で置換を行う
+        /// 置換文字列は "$C" + 条件の出現順（1～） + "-" + 後方参照の番号
+        /// 例： $C1-2（一つ目の条件となるタイマーの二つ目のグループに置換）
+        /// </summary>
+        /// <param name="message">元の文字列</param>
+        /// <param name="timers">置換候補のタイマー</param>
+        /// <param name="baseIndex">条件の出現順の開始番号</param>
+        /// <returns>候補となったタイマーの個数</returns>
+        private static int ReplaceMessageWithTelop(
+            StringBuilder message,
+            Guid[] timers,
+            int baseIndex)
+        {
+            int count = 0;
+
+            for (int i = 0; i < timers.Length; i++)
+            {
+                var telop = OnePointTelopTable.Default.GetOnePointTelopByGuid(timers[i]);
+                if (telop != null)
+                {
+                    count++;
+
+                    if (telop != null && telop.RegexEnabled && telop.Regex != null && !string.IsNullOrEmpty(telop.MatchedLog))
+                    {
+                        foreach (Match match in telop.Regex.Matches(telop.MatchedLog))
+                        {
+                            foreach (var number in telop.Regex.GetGroupNumbers())
+                            {
+                                message.Replace(String.Format("$C{0}-{1}", (baseIndex + i), number), match.Groups[number].Value);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
         }
     }
 }
