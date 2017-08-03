c4dd970b80f342a811f51b6b81d40ecdc023acfc
anoyetta <anoyetta@gmail.com>
2017-08-03 09:17:20 +0900

    アイコン等を整えた

--- XIVDBDownloader/Properties/Resources.Designer.cs
+++ XIVDBDownloader/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace XIVDBDownloader.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   型 System.Drawing.Bitmap のローカライズされたリソースを検索します。
+        /// </summary>
+        internal static System.Drawing.Bitmap Download {
+            get {
+                object obj = ResourceManager.GetObject("Download", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
