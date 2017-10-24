3e0892c65894376aa4d134bbf20894264ccfab9c
anoyetta <anoyetta@gmail.com>
2017-10-25 07:54:02 +0900

    patch 4.11

--- ACT.SpecialSpellTimer.Core/Properties/AssemblyInfo.cs
+++ ACT.SpecialSpellTimer.Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a71552bd-0ca6-48b2-bdb2-66447aef9c99")]
-[assembly: AssemblyVersion("1.22.0.3")]
-[assembly: AssemblyFileVersion("1.22.0.3")]
+[assembly: AssemblyVersion("1.22.0.4")]
+[assembly: AssemblyFileVersion("1.22.0.4")]
