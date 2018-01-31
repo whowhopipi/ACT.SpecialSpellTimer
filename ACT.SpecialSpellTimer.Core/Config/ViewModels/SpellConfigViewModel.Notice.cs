5b203579b4d5bf9f224401eafceaad29eed54355
anoyetta <anoyetta@gmail.com>
2018-01-31 18:42:22 +0900

    スペルの実装完了

--- ACT.SpecialSpellTimer.Core/Config/ViewModels/SpellConfigViewModel.Notice.cs
+++ ACT.SpecialSpellTimer.Core/Config/ViewModels/SpellConfigViewModel.Notice.cs
@@ -1,9 +1,22 @@
 using ACT.SpecialSpellTimer.Sound;
+using Prism.Commands;
+using System.Windows.Input;
 
 namespace ACT.SpecialSpellTimer.Config.ViewModels
 {
     public partial class SpellConfigViewModel
     {
         public static SoundController.WaveFile[] WaveList => SoundController.Instance.EnumlateWave();
+
+        private ICommand testSequencialTTSCommand;
+
+        public ICommand TestSequencialTTSCommand =>
+            this.testSequencialTTSCommand ?? (this.testSequencialTTSCommand = new DelegateCommand(() =>
+            {
+                this.Model.Play("おしらせ1");
+                this.Model.Play("おしらせ2");
+                this.Model.Play("おしらせ3");
+                this.Model.Play("おしらせ4");
+            }));
     }
 }
