3075a9c8057af11bd7b539f4602df425f9024483
anoyetta <anoyetta@gmail.com>
2018-05-31 17:02:33 +0900

    Json.NETのバージョンを変えた

--- ACT.SpecialSpellTimer/AssemblyResolver.cs
+++ ACT.SpecialSpellTimer/AssemblyResolver.cs
@@ -69,6 +69,7 @@ namespace ACT.SpecialSpellTimer
                 if (!this.Directories.Any(x => x == pluginDirectory))
                 {
                     this.Directories.Add(pluginDirectory);
+                    this.Directories.Add(Path.Combine(pluginDirectory, "references"));
                 }
             }
 
