60c83ed436ca0a03b6162fab07bdeae14bbc3765
anoyetta <anoyetta@gmail.com>
2018-06-13 14:55:31 +0900

    各種汎用アイコンを追加した

--- ACT.SpecialSpellTimer.Core/Views/LPSView.xaml.cs
+++ ACT.SpecialSpellTimer.Core/Views/LPSView.xaml.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Threading;
 using ACT.SpecialSpellTimer.Config;
+using FFXIV.Framework.Common;
 using FFXIV.Framework.WPF.Views;
 using Prism.Mvvm;
 
@@ -20,9 +24,10 @@ namespace ACT.SpecialSpellTimer.Views
 
         public static void ShowLPS()
         {
-            instance = new LPSView();
-
-            instance.OverlayVisible = Settings.Default.LPSViewVisible;
+            instance = new LPSView()
+            {
+                OverlayVisible = Settings.Default.LPSViewVisible
+            };
 
             instance.Show();
         }
@@ -93,12 +98,16 @@ namespace ACT.SpecialSpellTimer.Views
             this.timer.Interval = TimeSpan.FromSeconds(3.1);
             this.timer.Tick += this.Timer_Tick;
             this.timer.Start();
+
+            this.StartZOrderCorrector();
         }
 
         private void LPSView_Closed(object sender, EventArgs e)
         {
             this.timer.Stop();
             this.timer = null;
+
+            this.StopZOrderCorrector();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -114,6 +123,113 @@ namespace ACT.SpecialSpellTimer.Views
                 Visibility.Visible :
                 Visibility.Collapsed;
         }
+
+        #region ZOrder Corrector
+
+        private DispatcherTimer zorderCorrector;
+
+        private void StartZOrderCorrector()
+        {
+            this.zorderCorrector = new DispatcherTimer(DispatcherPriority.Background)
+            {
+                Interval = TimeSpan.FromSeconds(1),
+            };
+
+            this.zorderCorrector.Tick += (x, y) =>
+            {
+                if (this.OverlayVisible)
+                {
+                    if (!this.IsOverlaysGameWindow())
+                    {
+                        this.EnsureTopMost();
+                    }
+                }
+            };
+
+            this.zorderCorrector.Start();
+        }
+
+        private void StopZOrderCorrector()
+        {
+            if (this.zorderCorrector != null)
+            {
+                this.zorderCorrector.Stop();
+                this.zorderCorrector = null;
+            }
+        }
+
+        public IntPtr Handle => new WindowInteropHelper(this).Handle;
+
+        private bool IsOverlaysGameWindow()
+        {
+            var xivHandle = GetGameWindowHandle();
+            var handle = this.Handle;
+
+            while (handle != IntPtr.Zero)
+            {
+                // Overlayウィンドウよりも前面側にFF14のウィンドウがあった
+                if (handle == xivHandle)
+                {
+                    return false;
+                }
+
+                handle = NativeMethods.GetWindow(handle, NativeMethods.GW_HWNDPREV);
+            }
+
+            // 前面側にOverlayが存在する、もしくはFF14が起動していない
+            return true;
+        }
+
+        private void EnsureTopMost()
+        {
+            NativeMethods.SetWindowPos(
+                this.Handle,
+                NativeMethods.HWND_TOPMOST,
+                0, 0, 0, 0,
+                NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOACTIVATE);
+        }
+
+        private static object xivProcLocker = new object();
+        private static Process xivProc;
+        private static DateTime lastTry;
+        private static TimeSpan tryInterval = new TimeSpan(0, 0, 15);
+
+        private static IntPtr GetGameWindowHandle()
+        {
+            lock (xivProcLocker)
+            {
+                try
+                {
+                    // プロセスがすでに終了してるならプロセス情報をクリア
+                    if (xivProc != null && xivProc.HasExited)
+                    {
+                        xivProc = null;
+                    }
+
+                    // プロセス情報がなく、tryIntervalよりも時間が経っているときは新たに取得を試みる
+                    if (xivProc == null && DateTime.Now - lastTry > tryInterval)
+                    {
+                        xivProc = Process.GetProcessesByName("ffxiv").FirstOrDefault();
+                        if (xivProc == null)
+                        {
+                            xivProc = Process.GetProcessesByName("ffxiv_dx11").FirstOrDefault();
+                        }
+
+                        lastTry = DateTime.Now;
+                    }
+
+                    if (xivProc != null)
+                    {
+                        return xivProc.MainWindowHandle;
+                    }
+                }
+                catch (System.ComponentModel.Win32Exception) { }
+
+                return IntPtr.Zero;
+            }
+        }
+
+        #endregion ZOrder Corrector
     }
 
     public class LPSViewModel :
